4f9afcee2618fec773a8eea098be6ed3cf579ba8
LeeSeongHyeong <lshgooes@vineinc.co.kr>
2022-12-27 16:04:31 +0900

    깃 저장소 초기화

---
+++ routes/home/home.ctrl.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const home = (req, res) =>{
+    res.render("home/index");
+};
+
+const login = (req, res)=>{
+    res.render("home/login");
+};
+
+module.exports = {
+    home,
+    login,
+};
+
+
